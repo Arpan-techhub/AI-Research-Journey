27e080f5ebf5fe669b0dcc2f57725580db09464e
Arpan-techhub <iamarpanchk011@gmail.com>
2026-06-25 14:59:17 +0530

    Update day1_statistics_program.py

--- Month-1/Week-1/day1_statistics_program.py
+++ Month-1/Week-1/day1_statistics_program.py
@@ -1,1 +1,11 @@
+# take 5 numbers as input and calcukate the maximum, minimum and mean
+numbers=[]
+for i in range(5):
+  num=int(input("Enter a number:"))
+  numbers.append(num)
+  i=i+1
+print(min(numbers))
+print(max(numbers))
+mean=sum(numbers)/len(numbers)
+print(mean)
 
